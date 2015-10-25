6a89c3a96360b0f70c69c79f21d83c0b36fda403
Gary Fredericks <fredericksgary@gmail.com>
2015-10-25 02:19:43 +0000

    I think I'm getting somewhere

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -1,12 +1,24 @@
 (ns foreclojure.data-set
   (:use [foreclojure.fake-mongo]))
 
+;;
+;; Should the descriptions aim to actually introduce new concepts, or
+;; just describe the problem and leave the cheatsheet for that?
+;;
+;; I'm leaning toward the latter
+;;
+
 (def the-problems
   [{:title "Constancy is its own reward"
     :description "`(gen/return x)` is a generator that always generates `x`"
     :tags ["elementary"]
     :tests ["(gen/generator? __)"
-            "(every? #(= % 42) (gen/sample __ 1000))"]}])
+            "(->> (gen/sample __ 1000) (every? #(= % 42)))"]
+    :possible-answers '[(gen/return 42)]}
+   {:title "Numbers!"
+    :description "Generate some numbers"
+    :tags ["elementary"]
+    :tests ["(->> (gen/sample __ 1000) (every? integer?))"]}])
 
 (defn load-problems []
   (do
@@ -19,3 +31,26 @@
                       :_id id
                       :times-solved 0
                       :approved true)))))
+
+
+(comment
+
+  ;; working out tests
+
+  (require '[clojure.test.check.generators :as gen])
+
+  (defn avg [x y] (/ (+ x y) 2))
+
+  (defn recommend-bounds
+    [gen pred sample-count]
+    (let [[the-min the-max]
+          (->> (repeatedly 1000 (fn []
+                                  (->> (gen/sample gen sample-count)
+                                       (filter pred)
+                                       (count))))
+               (map #(/ % sample-count))
+               (apply (juxt min max)))]
+      [(avg 0 the-min) (avg the-max 1)]))
+
+
+  )
